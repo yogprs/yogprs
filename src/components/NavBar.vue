a47019d5be6baddc056c9fb72a1ba7410d4e888e
Fush1ren <nightingale.ox@gmail.com>
2025-09-29 17:21:04 +0700

    initial vue

---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { computed, ref } from "vue";
+
+const isHamburgerOpen = ref<boolean>(false);
+
+const hamburgerOpen = computed((): string => {
+  if (isHamburgerOpen)
+    return "https://img.icons8.com/?size=100&id=46&format=png&color=ffffff";
+  return "https://img.icons8.com/?size=100&id=3096&format=png&color=ffffff";
+});
+
+const hamburgerClick = (): void => {
+  isHamburgerOpen.value = !isHamburgerOpen.value;
+};
+</script>
+<template>
+  <div class="w-full py-4 px-12">
+    <div class="flex justify-between">
+      <span class="text-2xl text-white">Yogprs</span>
+      <img :src="hamburgerOpen" @click="hamburgerClick" class="w-8" />
+    </div>
+  </div>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  }
+})
