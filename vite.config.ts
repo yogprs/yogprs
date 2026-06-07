0b2027efaeb11a0684c47ef4c1eab2983011f216
Yoga Prasetya <nightingale.ox@gmail.com>
2026-06-07 20:26:56 +0700

    remove base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: "/",
+  // base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
